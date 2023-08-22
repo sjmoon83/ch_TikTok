44b9f0022211d40df009f9bc560c4c48279a9130
sjmoon83 <chachaprint@naver.com>
2023-08-22 16:28:08 +0900

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chtiktok/constants/sizes.dart';
+import 'package:chtiktok/screens/sign_up/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TickTokApp());
+}
+
+class TickTokApp extends StatelessWidget {
+  const TickTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFE9435A),
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: Sizes.size16 + Sizes.size2,
+                fontWeight: FontWeight.w600),
+          )),
+      home: const SignUpScreen(),
+    );
+  }
+}
